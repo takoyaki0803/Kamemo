9f5d345472e9c93817648db661ad7620839b2650
takoyaki0803 <yowo.omoshiroku25@gmail.com>
2016-04-28 12:47:56 +0900

    Update DbDefinition.swift

--- 160111/DbDefinition.swift
+++ 160111/DbDefinition.swift
@@ -3,7 +3,7 @@
 //  160111
 //
 //  Created by 20150301 on 2/11/16.
-//  Copyright © 2016 YasuhiroSugisawa. All rights reserved.
+//  Copyright © 2016 ******. All rights reserved.
 //
 
 //格納用DBの選択enum
